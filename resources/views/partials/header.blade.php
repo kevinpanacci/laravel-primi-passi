2495468a4343b85db86b652b22c3170b111b6123
Kevin <kevinpanacci@hotmail.it>
2020-05-05 19:45:03 +0200

    Iniziando la grafica

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <img id="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="logo"> {{-- nb: asset riporta direttamente alla cartella public, non va inserito il percorso fino a qui --}}
+    <nav class="header-nav">
+        <ul class="main-menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Prodotti</a></li>
+          <li><a href="#">Contatti</a></li>
+        </ul>
+    </nav>
+</header>
